414404983307a1f01549a646c9399b5b5357e53d
Evertt <Evert@Everts-Air.lan>
2015-04-14 23:15:50 +0200

    HtmlNode added

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ The following nodes are included:
 * `+` is for the `IncludeNode` and it includes another template into the current template.
 * `=` is for the `VariableNode` and it will insert the value of a variable, encoded by htmlentities. To avoid encoding by htmlentities you can use `==`.
 * `|` is for the `TextNode` and it will just print text, encoded with htmlentities.
+* `<` is for the `HtmlNode` and it will print HTML as is, only with variables replaced by their values.
 * `_` is for the `ExtendNode`. With this a view can extend a master view. **(Not yet implemented)**
 * `-` is for the `YieldNode` and it should yield a predefined section. **(Not yet implemented)**
 * `@` is for the `SectionNode`. It should assign its children to a section. **(Not yet implemented)**
---
+++ Slade/nodes/HtmlNode.php
@@ -0,0 +1,29 @@
+<?php namespace Slade\Nodes;
+
+use Slade\Scope;
+use Slade\Parser;
+
+/**
+ * @node /^</
+ */
+class HtmlNode extends Node {
+
+    public static function parse($node, Scope $scope, $inner) {
+        $node .= PHP_EOL . $inner . PHP_EOL;
+        
+        static::replaceVars($node, $scope);
+
+        return $node;
+    }
+
+    protected static function replaceVars(&$node, Scope $scope) {
+        preg_match_all('/{{\s*(\w+)\s*}}/', $node, $escapedVars);
+        foreach($escapedVars[1] as $i => $var)
+            $node = str_replace($escapedVars[0][$i], he($scope->get($var)), $node);
+
+        preg_match_all('/{!\s*(\w+)\s*!}/', $node, $unescapedVars);
+        foreach($unescapedVars[1] as $i => $var)
+            $node = str_replace($unescapedVars[0][$i], $scope->get($var), $node);
+    }
+
+}
